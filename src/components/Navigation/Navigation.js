6e4236781874840ec19debc44bf0e4b186a722d4
amitGithub14 <amit_rathore007@yahoo.com>
2020-07-09 20:54:15 +0530

    added component folder and navigation files

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "tachyons";
+
+const Navigation = () => {
+  return (
+    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+      <p>Logout</p>
+    </nav>
+  );
+};
+
+export default Navigation;
