a6303da8601608c8b1f7881d8900389b7da48b78
amitGithub14 <amit_rathore007@yahoo.com>
2020-07-26 21:10:51 +0530

    Updates

---
+++ precache-manifest.e0f0daf604b915a3ade7924c54cc80eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6b30659d2437fd1fc4798040a6911a",
+    "url": "/facefocus/index.html"
+  },
+  {
+    "revision": "b6e10545b5f4919710a4",
+    "url": "/facefocus/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9ebc0cde7636ecc99522",
+    "url": "/facefocus/static/css/main.7374f896.chunk.css"
+  },
+  {
+    "revision": "b6e10545b5f4919710a4",
+    "url": "/facefocus/static/js/2.485e4613.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facefocus/static/js/2.485e4613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ebc0cde7636ecc99522",
+    "url": "/facefocus/static/js/main.4dfa572e.chunk.js"
+  },
+  {
+    "revision": "635305b34c0bba8ce855",
+    "url": "/facefocus/static/js/runtime-main.bd7e292c.js"
+  }
+]);
